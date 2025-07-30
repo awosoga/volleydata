6de02f3ed7bfeac5506064e4d979502a6421480a
Ryan <ryandu343@gmail.com>
2025-07-30 10:44:21 -0400

    Add functions to upload data to github releases

--- initial/initialize_aupvb.py
+++ initial/initialize_aupvb.py
@@ -9,6 +9,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'utils'))
 from extract_aupvb_data import *
 from helpers import *
+from upload_to_releases import upload_to_releases
 
 def initialize_aupvb_player_info():
     """
@@ -25,9 +26,9 @@ def initialize_aupvb_player_info():
     for season in list(range(2021, year)):
         season_id = get_season_id(season)
         for game_number in range(1, 31):
-            url = f"https://auprosports.com/proxy.php?request=/api/stats/v2/volleyball/{season_id}/by-game/{game_number}?statTypes=volleyball"
+            url = f'https://auprosports.com/proxy.php?request=/api/stats/v2/volleyball/{season_id}/by-game/{game_number}?statTypes=volleyball'
             headers = {
-                "User-Agent": "Mozilla/5.0"
+                'User-Agent': 'Mozilla/5.0'
             }
             response = requests.get(url, headers=headers)
             json = response.json()
@@ -38,6 +39,7 @@ def initialize_aupvb_player_info():
 
     player_info = clean_aupvb_player_info(player_info)
     player_info.to_csv('aupvb_player_info.csv', index=False)
+    upload_to_releases('aupvb_player_info.csv', 'aupvb-player-info')
 
 
 def initialize_aupvb_leaderboards():
@@ -55,9 +57,9 @@ def initialize_aupvb_leaderboards():
     for season in list(range(2021, year)):
         season_id = get_season_id(season)
         for game_number in range(1, 31):
-            url = f"https://auprosports.com/proxy.php?request=/api/leaderboard/v2/volleyball/type/game?gameNumber={game_number}%26season={season_id}"
+            url = f'https://auprosports.com/proxy.php?request=/api/leaderboard/v2/volleyball/type/game?gameNumber={game_number}%26season={season_id}'
             headers = {
-                "User-Agent": "Mozilla/5.0"
+                'User-Agent': 'Mozilla/5.0'
             }
             response = requests.get(url, headers=headers)
             json = response.json()
@@ -66,6 +68,7 @@ def initialize_aupvb_leaderboards():
 
     leaderboards = clean_aupvb_leaderboards(leaderboards)
     leaderboards.to_csv('aupvb_leaderboards.csv', index=False)
+    upload_to_releases('aupvb_leaderboards.csv', 'aupvb-leaderboards')
 
 
 def initialize_aupvb_pbp():
@@ -84,9 +87,9 @@ def initialize_aupvb_pbp():
 
         pbp = pd.DataFrame()
         for game_number in range(1, 31):
-            url = f"https://auprosports.com/proxy.php?request=/api/play-by-play/v2/volleyball/event/{season_id}/{game_number}"
+            url = f'https://auprosports.com/proxy.php?request=/api/play-by-play/v2/volleyball/event/{season_id}/{game_number}'
             headers = {
-                "User-Agent": "Mozilla/5.0"
+                'User-Agent': 'Mozilla/5.0'
             }
             response = requests.get(url, headers=headers)
             json = response.json()
@@ -97,4 +100,10 @@ def initialize_aupvb_pbp():
             pbp = pd.concat([pbp, new_pbp], ignore_index=True)
 
         pbp = clean_aupvb_pbp(pbp)
-        pbp.to_csv(f'aupvb_pbp_{season}.csv', index=False)
+        file_name = 'aupvb_pbp_' + str(season) + '.csv'
+        pbp.to_csv(file_name, index=False)
+        upload_to_releases(file_name, 'aupvb-pbp')
+
+
+initialize_aupvb_leaderboards()
+initialize_aupvb_pbp()
